1356841a62c8b7094413ad16d58f0e7cb40f14df
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2026-08-05 16:39:37 -0400

    CHORE: Extend LoggedRobot and add at logger configuration

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -4,11 +4,17 @@
 /**************************************************************/
 package com.stuypulse.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import org.wpilib.command3.Command;
 import org.wpilib.command3.Scheduler;
 import org.wpilib.driverstation.Alliance;
 import org.wpilib.driverstation.MatchState;
-import org.wpilib.framework.TimedRobot;
 
 import java.util.Optional;
 
@@ -19,7 +25,7 @@ import java.util.Optional;
  * extends TimedRobot, meaning that the methods in this class are called automatically during
  * specific states of the robot.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     /**
      * Checks the alliance the robot is on
      *
@@ -41,6 +47,18 @@ public class Robot extends TimedRobot {
     public Robot() {
         robot = new RobotContainer();
         defaultScheduler = Scheduler.getDefault();
+
+        if (isReal()) {
+            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+            Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+        } else {
+            setUseTiming(false); // Run as fast as possible
+            String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+            Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+            Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+        }
+
+        Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
     }
 
     /*************************/
