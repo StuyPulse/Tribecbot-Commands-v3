b63b68832d219ad0acf5d3204c7f124f4010f86f
ivanlin20110421-lang <ivanlin20110421@gmail.com>
2026-08-20 16:20:38 -0400

    Feat: finished LEDSim

--- src/main/java/com/stuypulse/robot/subsystems/leds/LEDIOSim.java
+++ src/main/java/com/stuypulse/robot/subsystems/leds/LEDIOSim.java
@@ -1,37 +1,54 @@
+/************************ PROJECT TRIBECBOT *************************/
+/* Copyright (c) 2026 StuyPulse Robotics. All rights reserved. */
+/* Use of this source code is governed by an MIT-style license */
+/* that can be found in the repository LICENSE file.           */
+/***************************************************************/
 package com.stuypulse.robot.subsystems.leds;
 
-import org.wpilib.simulation.AddressableLEDSim;
+import static org.wpilib.units.Units.Amps;
+import static org.wpilib.units.Units.Celsius;
+import static org.wpilib.units.Units.Volts;
+
+import org.wpilib.hardware.led.AddressableLED;
+import org.wpilib.hardware.led.AddressableLEDBuffer;
+
+import com.ctre.phoenix6.signals.RGBWColor;
 
 public class LEDIOSim implements LEDIO {
-    private final AddressableLEDSim ledSim;
-    private final byte[] ledData = new byte[4];
+  private final AddressableLED led;
+  private final AddressableLEDBuffer buffer;
 
-    private final byte red = 0;
-    private final byte green = 0;
-    private final byte blue = 0;
-    private final byte white = 0;
+  public boolean isInitialized = false;
 
-    public boolean isInitialized = false;
+  public LEDIOSim() {
+    this.led = new AddressableLED(LEDConstants.Ports.LED_PORT);
+    this.buffer = new AddressableLEDBuffer(LEDConstants.Settings.LED_LENGTH);
 
-    public LEDIOSim() {
-        ledSim = new AddressableLEDSim(LEDConstants.Ports.CANDLE_PORT);
+    led.setLength(buffer.getLength());
+    led.setData(buffer);
+  }
 
-        ledSim.setInitialized(isInitialized);
-        ledSim.setData(new byte[]{red, green, blue, white});
-        ledSim.setLength(LEDConstants.Settings.LED_LENGTH);
-        ledSim.setStart(0);
+  @Override
+  public void updateInputs(LEDIOInputs inputs) {
+    inputs.isConnected = true;
+    inputs.supplyVoltage = Volts.of(12.0);
+    inputs.fiveVRailVoltage = Volts.of(5.0);
+    inputs.outputCurrentAmps = Amps.of(0.5);
+    inputs.LEDTemperature = Celsius.of(25.0);
+    inputs.hardwareFault = false;
+    inputs.underVoltageFault = false;
+  }
+
+  public void setOutputs(LEDIOOutputs outputs) {
+    for (LEDPattern pattern : outputs.patterns) {
+      RGBWColor color = pattern.color();
+
+      int start = Math.max(pattern.start(), 0);
+      int end = Math.min(pattern.end(), buffer.getLength() - 1);
+
+      for (int i = 0; i < buffer.getLength(); i++) {
+        buffer.setRGB(i, color.Red, color.Green, color.Blue);
+      }
     }
-
-    
-@Override
-public void updateInputs(LEDIOInputs inputs) {
-    inputs.isConnected = ledSim.getInitialized();
-    }
-
-public void setOutputs(LEDIOOutputs outputs) {
-    ledSim.setData(new byte[]{(byte) outputs.color.Red, 
-        (byte) outputs.color.Green, 
-        (byte) outputs.color.Blue, 
-        (byte) outputs.color.White});
-    }
+  }
 }
